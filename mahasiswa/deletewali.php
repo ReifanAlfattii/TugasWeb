456f36a472a0c52d27e2975ab9328a03ca26b077
ReifanAlfattii <140674005+ReifanAlfattii@users.noreply.github.com>
2023-11-24 16:39:38 +0700

    Delete mahasiswa directory

--- mahasiswa/deletewali.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include 'koneksi.php';
-    $id = $_GET['id'];
-    $query = "DELETE from wali where id='$id'";
-    mysqli_query($koneksi, $query);
-    header("location:index.php");
-?>
